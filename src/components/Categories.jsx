72fe5eda4a130c2c7b4bab4955473d06abb0c005
iamakitten1 <m.kostamashvili.1@gmail.com>
2024-08-28 14:00:15 +0200

    change

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,13 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export function Categories() {
     const [content, setContent] = useState(<CategoriesList showForm={showForm} />);
     
-    function showList(){
+    function showList() {
         setContent(<CategoriesList showForm={showForm} />);
     }
 
-    function showForm(){
+    function showForm() {
         setContent(<CategoriesForm showList={showList} />);
     }
 
@@ -19,11 +19,38 @@ export function Categories() {
 }
 
 function CategoriesList({ showForm }) {
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        fetchCategories();
+    }, []);
+
+    async function fetchCategories() {
+        const url = "http://localhost:3000/categories";
+        try {
+            const response = await fetch(url);
+            if (!response.ok) {
+                throw new Error(`Response status: ${response.status}`);
+            }
+
+            const json = await response.json();
+            setCategories(json); // Set the fetched categories to state
+            console.log(json);
+        } catch (error) {
+            console.error(error.message);
+        }
+    }
+
     return (
         <>
             <h2 className="text-center mb-3">List of Categories</h2>
             <button onClick={showForm} type="button" className="btn btn-primary me-2">Create</button>
-        </>   
+            <ul>
+                {categories.map((category) => (
+                    <li key={category.id}>{category.name}</li>
+                ))}
+            </ul>
+        </>
     );
 }
 
